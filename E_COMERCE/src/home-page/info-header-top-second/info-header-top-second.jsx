e10d82eea8078f541201279891e076546a00ef0e
VadimVasilcovan <152361519+VadimVasilcovan@users.noreply.github.com>
2024-11-20 14:34:12 +0200

    burger menu

--- E_COMERCE/src/home-page/info-header-top-second/info-header-top-second.jsx
+++ E_COMERCE/src/home-page/info-header-top-second/info-header-top-second.jsx
@@ -1,18 +1,35 @@
-import React from 'react'
-import './info-header-top-second.css'
+import React, { useState } from 'react';
+import './info-header-top-second.css';
 import HomeBtn from '../atoms/home-button/home-btn';
 import CarsBtn from '../atoms/cars-button/cars-btn';
 import AboutUsBtn from '../atoms/about-us-button/about-us-btn';
 import ContactBtn from '../atoms/contact-button/contact-btn';
 import InputSearch from '../atoms/input-search/input';
 
-function InfoHeaderTopSecond(){
-    return<div className='info-Header-Top-Second'>
-        <HomeBtn/>
-        <CarsBtn/>
-        <AboutUsBtn/>
-        <ContactBtn/>
-        <InputSearch/>
-    </div>
+function InfoHeaderTopSecond() {
+    const [menuOpen, setMenuOpen] = useState(false);
+
+    const toggleMenu = () => {
+        setMenuOpen(!menuOpen);
+    };
+
+    return (
+        <div className="info-Header-Top-Second">
+            <div className={`menu ${menuOpen ? 'show' : ''}`}>
+                <HomeBtn />
+                <CarsBtn />
+                <AboutUsBtn />
+                <ContactBtn />
+            </div>
+            {/* Position search input outside of the menu */}
+            <div className={`search-container ${menuOpen ? 'left' : ''}`}>
+                <InputSearch />
+            </div>
+            <button className="burger-menu" onClick={toggleMenu}>
+                &#9776; {/* Hamburger Icon */}
+            </button>
+        </div>
+    );
 }
+
 export default InfoHeaderTopSecond;
